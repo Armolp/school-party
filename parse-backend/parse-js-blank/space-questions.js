97003878111671163d70a67d4cff1004c74cb9df
Diego Ponce <diegoponce@outlook.com>
2015-11-21 01:09:11 -0600

    Parse gets and fetch works

--- parse-backend/parse-js-blank/space-questions.js
+++ parse-backend/parse-js-blank/space-questions.js
@@ -1,6 +1,46 @@
 Parse.initialize("AK26J8RDJqlwxb2wewzz4sE7dpvYJOdVstu7v6be", "RrkuVkdBBws0vADmr1kb8SkGGu1aZm49g9qsUJiX");
 
-function questions(){
+function getData() {
+  var SpaceQuestion = Parse.Object.extend("SpaceQuestion");
+  var query = new Parse.Query(SpaceQuestion);
+
+  //Get all data
+  var query = new Parse.Query(SpaceQuestion);
+  query.find({
+    success: function(data) {
+      for (var i in data) {
+        // console.log(data[i].id);
+        query.get(data[i].id, {
+          success: function(spaceQuestion) {
+            // The object was retrieved successfully.
+            var question = spaceQuestion.get("question");
+            var a = spaceQuestion.get("a");
+            var b = spaceQuestion.get("b");
+            var c = spaceQuestion.get("c");
+            var answer = spaceQuestion.get("answer");
+            console.log(question + "\n" + a + "\n" + b + "\n" + c + "\n" + answer);
+
+            var para = document.createElement("p");
+            var node = document.createTextNode(question + "\n" + a + "\n" + b + "\n" + c + "\n" + answer);
+            para.appendChild(node);
+            var element = document.getElementById("info");
+            element.appendChild(para);
+          },
+          error: function(object, error) {
+            // The object was not retrieved successfully.
+            // error is a Parse.Error with an error code and message.
+          }
+        });
+      }
+    },
+    error: function(error) {
+      console.log(error);
+    }
+  });
+}
+
+
+function questions() {
 
   var SpaceQuestion = Parse.Object.extend("SpaceQuestion");
   var spaceQuestion = new SpaceQuestion();
@@ -14,7 +54,9 @@ function questions(){
   spaceQuestion.save(null, {
     success: function(spaceQuestion) {
       // Execute any logic that should take place after the object is saved.
-      alert('New object created with objectId: ' + spaceQuestion.id);
+      // alert('New object created with objectId: ' + spaceQuestion.id);
+      alert("Pregunta guardada");
+      window.location.reload();
     },
     error: function(spaceQuestion, error) {
       // Execute any logic that should take place if the save fails.
